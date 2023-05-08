527906e74dda72a360ae6f4fdf8a721c77291f50
Suely Souza Santos <adna_suellen@hotmail.com>
2023-05-08 00:51:32 -0300

    fix: consertar imagem

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import "./index.css"
 function App() {
   return (
     <div className="App">
-      <div className="image">
-        <img src="https://images.pexels.com/photos/14171630/pexels-photo-14171630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
-        <img src="public\pngwing.com.png" style={{position:'absolute'}}/>
+      <div className="image"  style={{backgroundColor: 'black'}}>
       </div>
       <div className="body"></div>
     </div>
