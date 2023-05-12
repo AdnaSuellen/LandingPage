d9da11af183089c334ce22cfc807d063fa24a28c
Suely Souza Santos <adna_suellen@hotmail.com>
2023-05-11 22:50:43 -0300

    fix: fazer a animação do título

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ function App() {
   return (
     <div className="App">
       <div className="image"  style={{backgroundColor: 'black'}}>
+      <h1 class="animate__animated animate__bounce" id="titulo" >Cake</h1>
       </div>
       <div className="body"></div>
     </div>
