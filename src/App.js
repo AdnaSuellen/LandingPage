5559896df7215767754c19cb67dcead9827c70d2
Suely Souza Santos <adna_suellen@hotmail.com>
2023-05-17 21:20:15 -0300

    feat: criado novo fundo

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ function App() {
   return (
     <div className="App">
       <div className="image"  style={{backgroundColor: 'black'}}>
-      <h1 class="animate__animated animate__bounce" id="titulo" >Cake</h1>
+      
       </div>
-      <div className="body"></div>
+      <div className="body">
+       
+      </div>
+      <div className="footer"></div>
     </div>
   );
 }
