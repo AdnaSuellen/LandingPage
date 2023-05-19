1e20f2fdac4cd57a7b0c04e007d6382aa079fe45
Suely Souza Santos <adna_suellen@hotmail.com>
2023-05-19 17:53:49 -0300

    finalizar o header

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import "./index.css"
 function App() {
   return (
     <div className="App">
+       
       <div className="image"  style={{backgroundColor: 'black'}}>
-      
+          <a href="#" className="head">HOME</a>
+          <a href="#" className="head">QUEM SOMOS</a>
+          <a href="#" className="head">PACOTES</a>
+          <a href="#" className="head">FALE CONOSCO</a>
       </div>
       <div className="body">
        
