3f53e717b9f80ba3ed6ed7f42e6a8ca7973cd1d8
Suely Souza Santos <adna_suellen@hotmail.com>
2023-05-20 12:18:45 -0300

    coloquei a loogo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import "./index.css"
+import "./index.css";
 function App() {
   return (
     <div className="App">
-       
-      <div className="image"  style={{backgroundColor: 'black'}}>
-     
-          <a href="#" className="head">HOME</a>
-          <a href="#" className="head">QUEM SOMOS</a>
-          <a href="#" className="head">PACOTES</a>
-          <a href="#" className="head">FALE CONOSCO</a>
+      <div className="image" style={{ backgroundColor: "black" }}>
+        <div className="container-inicio">
+          <div className="container">
+            <a href="#" className="head">
+              HOME
+            </a>
+            <a href="#" className="head">
+              QUEM SOMOS
+            </a>
+            <a href="#" className="head">
+              PACOTES
+            </a>
+            <a href="#" className="head">
+              FALE CONOSCO
+            </a>
+          </div>
+          <div className="titulo">
+            <img className="img-titulo" />
+          </div>
+        </div>
       </div>
-      <div className="body">
-       
-      </div>
+      <div className="body"></div>
       <div className="footer"></div>
     </div>
   );
