a312dab90955da26e34d307dbbde4838b494f03b
Adna Suellen <adna_suellen@hotmail.com>
2023-09-06 22:53:11 -0300

    finalizando

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
-import "./index.css";
-function App() {
+import { useState, useEffect } from "react"
+import { useRef } from "react"
+import Alert from "@mui/material/Alert"
+import AlertTitle from "@mui/material/AlertTitle"
+import Stack from "@mui/material/Stack"
+import "./index.css"
+import React from "react"
+import { Toast } from "primereact/toast"
+import { Button } from "primereact/button"
+
+const App = () => {
+  const [heart1, setHeart1] = useState(false)
+  const [heart2, setHeart2] = useState(false)
+  const [heart3, setHeart3] = useState(false)
+  const [alert1, setAlert1] = useState(false)
+  const toast = useRef(null)
+
+  const ChangeStateButtom1 = () => {
+    setHeart1(!heart1)
+    setAlert1(!alert1)
+  }
+
+  const ChangeStateButtom2 = () => {
+    setHeart2(!heart2)
+  }
+
+  const ChangeStateButtom3 = () => {
+    setHeart3(!heart3)
+  }
+
+  const Timer = setTimeout(() => {
+    setAlert1(true)
+  }, 1000)
+
   return (
-    <div className="App">
+    <>
       <div className="image" style={{ backgroundColor: "black" }}>
         <div className="container-inicio">
           <div className="container">
@@ -18,10 +50,10 @@ function App() {
               FALE CONOSCO
             </a>
           </div>
-          <div className="titulo-1">
+          {/* <div className="titulo-1">
             <h1 className="titulo-2">BELO</h1>
             <h2 className="titulo-3">DESTINO</h2>
-          </div>
+          </div> */}
           <div className="texto">
             <p className="texto1">
               Venha conosco e descubra o mundo através dos nossos pacotes.
@@ -34,25 +66,131 @@ function App() {
       </div>
       <div className="body"></div>
       <div className="main">
-        <div className="pacote-body">
-          <video
-            className="video-container"
-            autoplay=""
-            muted=""
-            plays-inline=""
-            loop
-            data-uia="nmhp-card-animation-asset-video"
-          >
-            <source
-              className="video"
-              src="cidade.mp4"
-              type="video/mp4"
-            ></source>
-          </video>
+        <div className="picture">
+          <div className="containerPic1">
+            <img
+              className="pic1"
+              src="https://images.pexels.com/photos/15889175/pexels-photo-15889175/free-photo-of-fofo-macio-peludo-panorama.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+            />
+            <div className="containerTextHeart">
+              <div className="containerText">
+                <p className="textPic1">Lorem Ipsum</p>
+                <p className="textPic2">AAA-OOO</p>
+                <p className="textPic3">R$ 000,00</p>
+              </div>
+              <div className="containerHeart">
+                {heart1 === false ? (
+                  <>
+                    <img
+                      className="heartImage"
+                      style={{ cursor: "pointer" }}
+                      name="heart1"
+                      value={heart1}
+                      onClick={ChangeStateButtom1}
+                      src="/heart1.png"
+                    />
+                  </>
+                ) : (
+                  <>
+                    <Alert
+                      style={{
+                        backgroundColor: "transparent",
+                        color: "white",
+                        border: "solid 1px rgba(255, 255, 255, 0.164)",
+                        marginBottom: "40vh",
+                        height: "60px",
+                      }}
+                      severity="success"
+                      defaultValue={Timer}
+                      onScroll={Timer}
+                    >
+                      Adicionado à lista de desejos.
+                    </Alert>
+                    <img
+                      className="heartImage"
+                      style={{ cursor: "pointer" }}
+                      value={heart1}
+                      onClick={ChangeStateButtom1}
+                      src="/heart2.png"
+                    />
+                  </>
+                )}
+              </div>
+            </div>
+          </div>
+          <div className="containerPic1">
+            <img
+              className="pic2"
+              src="https://images.pexels.com/photos/16913242/pexels-photo-16913242/free-photo-of-nuvem-terra-cultivada-plantacao-alvorecer.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            ></img>
+
+            <div className="containerTextHeart">
+              <div className="containerText">
+                <p className="textPic1">Lorem Ipsum</p>
+                <p className="textPic2">AAA-OOO</p>
+                <p className="textPic3">R$ 000,00</p>
+              </div>
+              <div className="containerHeart">
+                {heart2 === false ? (
+                  <img
+                    className="heartImage"
+                    style={{ cursor: "pointer" }}
+                    value={heart2}
+                    onClick={ChangeStateButtom2}
+                    src="/heart1.png"
+                  />
+                ) : (
+                  <>
+                    {" "}
+                    <img
+                      className="heartImage"
+                      style={{ cursor: "pointer" }}
+                      value={heart2}
+                      onClick={ChangeStateButtom2}
+                      src="/heart2.png"
+                    />
+                    <Toast ref={toast} />
+                  </>
+                )}
+              </div>
+            </div>
+          </div>
+          <div className="containerPic1">
+            <img
+              className="pic3"
+              src="https://images.pexels.com/photos/3254549/pexels-photo-3254549.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            ></img>
+            <div className="containerTextHeart">
+              <div className="containerText">
+                <p className="textPic1">Lorem Ipsum</p>
+                <p className="textPic2">AAA-OOO</p>
+                <p className="textPic3">R$ 000,00</p>
+              </div>
+              <div className="containerHeart">
+                {heart3 === false ? (
+                  <img
+                    className="heartImage"
+                    style={{ cursor: "pointer" }}
+                    value={heart3}
+                    onClick={ChangeStateButtom3}
+                    src="/heart1.png"
+                  />
+                ) : (
+                  <img
+                    className="heartImage"
+                    style={{ cursor: "pointer" }}
+                    value={heart3}
+                    onClick={ChangeStateButtom3}
+                    src="/heart2.png"
+                  />
+                )}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
